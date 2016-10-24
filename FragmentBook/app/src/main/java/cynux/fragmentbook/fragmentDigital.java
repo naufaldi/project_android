a5df35c33f0b19301b3216e6254378423bee44a1
naufaldi <naufaldi.rafif@gmail.com>
2016-10-24 23:07:21 +0700

    main_menu

---
+++ FragmentBook/app/src/main/java/cynux/fragmentbook/fragmentDigital.java
@@ -0,0 +1,20 @@
+package cynux.fragmentbook;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by cynux on 24/10/16.
+ */
+
+public class fragmentDigital extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_digital,container,false);
+    }
+}
---
+++ StaticFragment/app/src/main/java/cynux/staticfragment/MainActivity.java
@@ -0,0 +1,41 @@
+package cynux.staticfragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       // Fragment fragment ;
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_date:
+                startActivity(new Intent(Settings.ACTION_DATE_SETTINGS));
+                break;
+            case R.id.menu_location:
+                startActivity(new Intent(Settings.ACTION_LOCALE_SETTINGS));
+                break;
+            case R.id.menu_sleep:
+                startActivity(new Intent(Settings.ACTION_SOUND_SETTINGS));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
